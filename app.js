0c9bd4d6de9d9a43b73dbc158f051f9a02e26330
Puchana <puchana123@gmail.com>
2022-12-20 15:07:36 +0700

    Level 2 Data Encryption

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -15,6 +16,9 @@ const userSchema = new mongoose.Schema({
     email : String,
     password: String
 })
+
+const secret = "Thisisalittlesecrect"; // secret message use to encrypt data
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema);
 
