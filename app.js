7e1b17cf29e78f010a9de6b0f593376b5bd8e6bb
Puchana <puchana123@gmail.com>
2022-12-20 14:29:42 +0700

    Level 1 Email and Password

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB")
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password: String
+})
+
+const User = new mongoose.model("User",userSchema);
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.route('/login')
+
+    .post((req,res)=>{
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username},(err,foundUser)=>{
+            if(!err){
+                if(foundUser){
+                    if(foundUser.password === password){
+                        res.render('secrets');
+                    }
+                }
+            }else{
+                res.send(err);
+            }
+        })
+    })
+
+    .get((req,res)=>{
+        res.render('login');
+    })
+
+app.route('/register')
+    .get((req,res)=>{
+        res.render('register');
+    })
+
+    .post((req,res)=>{
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        })
+        newUser.save((err)=>{
+            if(!err){
+                res.render('secrets');
+            }else{
+                res.send(err);
+            }
+        });
+    })
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000");
+});
